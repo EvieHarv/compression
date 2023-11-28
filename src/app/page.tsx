2327958ed9d92481335f29bcc676287cc9ac6517
Ethan Harvey <57858587+EvieHarv@users.noreply.github.com>
2023-11-28 00:38:55 -0600

    landing page temp explanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,28 @@ export default function Home() {
     <StandardPageWrapper>
       <Heading>Compression</Heading>
       <hr />
-      <Par>(some intro blurb)</Par>
+      <Par>
+        This is a work in progress! I'm almost done with the "first section" of
+        huffman coding, but essentially this site aims to just give an
+        interactive and cool way of understanding different encodings.
+      </Par>
+      <Par>
+        The thing I've been working on for most of the time has been the huffman
+        coding example in the playground. It's surprisingly hard to render
+        arbitrary n-ary trees nicely! See{" "}
+        <StyledLink href="/playground/huffman">the experiment</StyledLink> and
+        the{" "}
+        <StyledLink href="https://github.com/EvieHarv/compression/blob/main/src/components/mafs/TreeVisualization.tsx">
+          main code used in it
+        </StyledLink>{" "}
+        to know what I mean.
+      </Par>
+      <Par>
+        So the site is pretty sparse, and what is here right now is very much
+        just a draft, but feel free to look around anyways!
+      </Par>
       <ul>
+        <li>WIP articles</li>
         <li>
           -{" "}
           <StyledLink href="/introduction">
@@ -19,6 +39,7 @@ export default function Home() {
         <li>
           - <StyledLink href="/huffman">huffman coding</StyledLink>
         </li>
+        <li>Dev stuff</li>
         <li>
           - <StyledLink href="/playground">dev playground</StyledLink>
         </li>
